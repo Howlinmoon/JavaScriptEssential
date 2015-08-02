062e638b8c332716058c3d08bdd16965a4f28a66
Howlinmoon <github@strifecrafter.com>
2015-08-02 10:58:49 -0400

    fixed assignment vs equality issue

--- Chapter_07/CommonErrors/script.js
+++ Chapter_07/CommonErrors/script.js
@@ -23,6 +23,7 @@ window.onload = function() {
 }
 */
 
+/*
 // Issue 2b: typos very common with DOM methods
 // fixed typo'd case for Id
 var x = document.getElementById("something");
@@ -32,24 +33,25 @@ var myArray = ["one","two","three"];
 //typo in 'reverse' fixed
 console.log(myArray.reverse());
 
-
+*/
 
 // Issue 4: using document.getElementById before the element is part of the DOM.
 // make sure the DOM is loaded - use window.load
-//var myImage = document.getElementById("someImage");
+var myImage = document.getElementById("someImage");
 
 // Issue 5 - assignment instead of equality
 // This is NOT technically an error
-/*
+
 var a = 10;
 var b = 20;
 
-if ( a = b) {
+// fixed the following equality vs assignment typo
+if ( a == b) {
 	console.log("Something is wrong with the universe.");
 } else {
 	console.log("This is what I expect!")
 }
-*/
+
 
 // Issue 6: missing parameters in function calls:
 // This is NOT an error - it's just an unexpected result.
