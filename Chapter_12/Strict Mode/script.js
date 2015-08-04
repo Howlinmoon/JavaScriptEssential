f3b8e6638f44e69558b56954844dd07dba23eb19
Jim Veneskey <github@strifecrafter.com>
2015-08-04 12:34:26 -0400

    Experimenting with 'use strict' or strict mode

---
+++ Chapter_12/Strict Mode/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// didn't use var
+var foo = "Hello world";
+
+// new object with two properties with the same name
+var myObject = { samename: 1, samename: 2 };
+
+// function with the same name for multiple parameters
+function myFunction( a,b,c) {
+   return a + b + c;
+}
+
+document.getElementById("mainContent").innerHTML = foo;
+
+
+
