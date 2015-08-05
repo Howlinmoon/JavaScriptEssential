d0398d719f3c7027a7e01519bb239686f7204a50
Jim Veneskey <github@strifecrafter.com>
2015-08-05 06:46:31 -0400

    Experimenting with creating objects (with methods and properties) the Javascript way

--- Chapter_13/ObjectsPrototypes/script.js
+++ Chapter_13/ObjectsPrototypes/script.js
@@ -1,12 +1,36 @@
 // Simple prototype example
 
-function Player(n) {
+// the original object constructor with properties
+function Player(n, s, r) {
     this.name = n;
+    this.score = s;
+    this.rank = r;
 }
 
+// add methods to the initial object design via prototype
+Player.prototype.logInfo = function() {
+    console.log("I am: ", this.name);
+}
 
-var fred =  new Player("Fred");
+Player.prototype.promote = function() {
+    this.rank++;
+    console.log("My new rank is: ", this.rank);
+}
 
+var fred =  new Player("Fred", 1000, 5);
+fred.logInfo();
+fred.promote();
+
+// create new instances
+var bob = new Player("Bob", 50, 1);
+bob.logInfo();
+bob.promote();
+bob.logInfo();
+bob.promote();
+
+var jane = new Player("Jane", 50000, 10);
+jane.logInfo();
+jane.promote();
 
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # JavaScriptEssential
 
 Continue here next time:
-http://www.lynda.com/JavaScript-tutorials/Working-objects-prototypes/81266/87576-4.html
+http://www.lynda.com/JavaScript-tutorials/Example-Countdown/81266/87578-4.html
